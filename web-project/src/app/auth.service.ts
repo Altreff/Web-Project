cf458b92bc22a9874e44331b31ad98af71873210
Shaken05 <122993715+Shaken05@users.noreply.github.com>
2024-04-26 01:55:59 +0500

    jwt

---
+++ web-project/src/app/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject, throwError } from 'rxjs';
+
+interface User {
+    username: string;
+    password: string;
+  }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  AUTH_SERVER: string = 'http://localhost:3000';
+  authSubject = new BehaviorSubject(false);
+
+  constructor(private httpClient: HttpClient) { }
+    
+  login(user: User): Observable<any> {
+    return this.httpClient.post<{ token: string }>(`${this.AUTH_SERVER}/login`, user).pipe(
+      tap((res: { token: string }) => {
+        if (res.token) {
+          localStorage.setItem('access_token', res.token);
+          this.authSubject.next(true);
+        } else {
+          // Обработка ошибки аутентификации
+          console.error('Ошибка аутентификации: недействительный токен');
+        }
+      }),
+      catchError((err) => {
+        // Обработка ошибок сети или сервера
+        console.error('Ошибка сети или сервера', err);
+        return throwError(err);
+      })
+    );
+  }
+  
+  
+
+  isLoggedIn() {
+    return this.authSubject.asObservable();
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    this.authSubject.next(false);
+  }
+}
--- web-project/src/app/login/login.component.ts
+++ web-project/src/app/login/login.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  user = {
+    username: 'user',
+    password: '1234'
+  };
 
+  constructor(private authService: AuthService) { }
+
+  onSubmit() {
+    this.authService.login(this.user).subscribe(
+      (res) => {
+        // Обработка успешного входа в систему
+        console.log('Успешный вход в систему!', res);
+      },
+      (err) => {
+        // Обработка ошибок аутентификации
+        console.error('Ошибка аутентификации', err);
+      }
+    );
+  }
 }
